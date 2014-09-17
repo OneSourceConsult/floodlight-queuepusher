7eecdf8b378526a549d9f3921842edd8551df45d
João Gonçalves <joagonca@onesource.pt>
2014-09-17 14:27:18 +0100

    Update Utils.java

--- net/floodlightcontroller/queuepusher/Utils.java
+++ net/floodlightcontroller/queuepusher/Utils.java
@@ -1,3 +1,15 @@
+/*
+ * @(#)Utils.java        1.00 2013
+ *
+ * Copyright (c) 2013 OneSource, Consultoria Informatica.
+ * All rights reserved.
+ *
+ * This software is copyright of OneSource Consultoria Informatica.
+ * You use it only in accordance with the terms of the
+ * license agreement you entered into
+ * with OneSource.
+ */
+
 package net.floodlightcontroller.queuepusher;
 
 import java.io.BufferedReader;
