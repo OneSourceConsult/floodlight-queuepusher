61ada9d7d81523b936d6c6a4f722c269315d8afc
João Gonçalves <joagonca@onesource.pt>
2014-09-17 14:25:58 +0100

    Update QueuePusherResponse.java

--- net/floodlightcontroller/queuepusher/QueuePusherResponse.java
+++ net/floodlightcontroller/queuepusher/QueuePusherResponse.java
@@ -4,10 +4,9 @@
  * Copyright (c) 2013 OneSource, Consultoria Informatica.
  * All rights reserved.
  *
- * This software is the confidential and proprietary information of OneSource
- * Consultoria Informatica. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
+ * This software is copyright of OneSource Consultoria Informatica.
+ * You use it only in accordance with the terms of the
+ * license agreement you entered into
  * with OneSource.
  */
 
